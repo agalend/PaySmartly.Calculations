4d3fc1b71fc8f38e2c9eb4df7c50ee0b53a9cbdb
stefan <stefan.bozov@outlook.com>
2023-11-30 18:25:30 +0200

    remove some endpoints

--- PaySmartly.Calculations/Endpoints/PaySlipEndpoints.cs
+++ PaySmartly.Calculations/Endpoints/PaySlipEndpoints.cs
@@ -2,8 +2,6 @@ namespace PaySmartly.Calculations.Endpoints
 {
     static class PaySlipEndpoints
     {
-        public static PaySlipEndpoint CreateEndpoint = new("/payslips", "createPaySlip", "POST");
-        public static PaySlipEndpoint GetEndpoint = new("/payslips/{id}", "getPaySlipById", "GET");
-        public static PaySlipEndpoint DeleteEndpoint = new("/payslips/{id}", "deletePaySlipById", "DELETE");
+        public static PaySlipEndpoint CreateEndpoint = new("/pay-slips", "create-pay-slip", "POST");
     }
 }
