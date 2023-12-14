e454bbb4247137d0bf265ac82db82339f18e4a26
stefan <stefan.bozov@outlook.com>
2023-12-14 06:18:49 +0200

    better superRate validation

--- PaySmartly.Calculations/Filters/CreatePaySlipValidator.cs
+++ PaySmartly.Calculations/Filters/CreatePaySlipValidator.cs
@@ -23,9 +23,9 @@ namespace PaySmartly.Calculations.Filters
             }
 
             double? superRate = userRequest?.SuperRate;
-            if (superRate is null || superRate < 0d)
+            if (superRate is null || (superRate < 0d || superRate > 50d))
             {
-                return Results.BadRequest($"Invalid {nameof(superRate)} value, you should provide a positive, bigger than 0 super rate");
+                return Results.BadRequest($"Invalid {nameof(superRate)} value, you should provide a positive, bigger than 0  and less than 50 super rate");
             }
 
             DateTime? payPeriodFrom = userRequest?.PayPeriodFrom;
